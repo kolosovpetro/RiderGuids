1450f9ae64532fb1275d7119c40dd41ed836f12b
kolosovpetro <kolosovp94@gmail.com>
2022-08-04 09:23:10 +0200

    initial commit

---
+++ src/dotnet/ReSharperPlugin.RiderGuids.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.RiderGuids.Tests
+{
+    [ZoneDefinition]
+    public class RiderGuidsTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IRiderGuidsZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<RiderGuidsTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class RiderGuidsTestsAssembly : ExtensionTestEnvironmentAssembly<RiderGuidsTestEnvironmentZone> { }
+}
